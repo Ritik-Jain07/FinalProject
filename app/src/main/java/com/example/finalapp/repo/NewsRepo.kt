229db84750d5a0d3d675f131fa6bb8b6b83b03cc
Ritik_Jain <ritik.jain@tothenew.com>
2021-05-04 18:40:58 +0530

    All changes has been done.

--- app/src/main/java/com/example/finalapp/repo/NewsRepo.kt
+++ app/src/main/java/com/example/finalapp/repo/NewsRepo.kt
@@ -13,6 +13,7 @@ class NewsRepo {
     val TAG = NewsRepo::class.java.simpleName
     private val KEY = "163d19643270bc5175c96f6475eda9f8"
     val mutableList: MutableLiveData<DataModel> = MutableLiveData()
+    val errorString: MutableLiveData<String> = MutableLiveData()
 
     /**
      * Function getData() to fetch API without keyword
@@ -22,6 +23,10 @@ class NewsRepo {
         val call = ApiClient.getClient.getNews(KEY, "en", category)
         call.enqueue(object : Callback<DataModel> {
             override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                if(response.code()!=200){               // Handling error API calls
+                    errorString.value=response.code().toString()
+                    return
+                }
                 Log.i(TAG, response.body().toString())
                 mutableList.postValue(response.body())
             }
@@ -42,6 +47,10 @@ class NewsRepo {
             val call = ApiClient.getClient.getKeywordNews(KEY, "en", category, keyword)
             call.enqueue(object : Callback<DataModel> {
                 override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
+                    if(response.code()!=200){                           // Handling error API calls
+                        errorString.value=response.code().toString()
+                        return
+                    }
                     Log.i(TAG, response.body().toString())
                     mutableList.postValue(response.body())
                 }
